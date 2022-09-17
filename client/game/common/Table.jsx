6aeb827aaa8b9d40000853216a2ca42ae8b4a1b9
Mohammed Alsobay <mosobay@mit.edu>
2022-09-17 11:49:19 -0400

    add factor for displaying other players' summaries

--- client/game/common/Table.jsx
+++ client/game/common/Table.jsx
@@ -26,34 +26,37 @@ export default class Table extends React.Component {
               const punishedBy = player.round.get("punishedBy");
               const contribution = player.round.get("contribution");
               const roundPayoff = player.round.get("roundPayoff");
-              return(
-                <tr className={i === 0 ? 'tr back-gray' : 'tr'} key={i}>
-                  <td className='td' >
-                    <img src={player.get("avatar")} className="avatar" />
-                  </td>
-                  <td className='td'><h2>{contribution}</h2></td>
-                  {punishment && 
-                  <>
-                    <td className='td'>
-                      <ListView
-                        punishments={punished}
-                        game={game}
-                        me={me}
-                      />
-                    </td>
-                    <td className='td' >
-                      <ListView
-                        punishments={punishedBy}
-                        game={game}
-                        me={me}
-                      />
-                    </td>
-                  </>
-                  } 
-                  <td className='td'><font color={roundPayoff > 0 ? "green":"red"}><h2>{roundPayoff}</h2></font></td>  
-                </tr>
-              )
-            })}
+              if (i == 0 || game.treatment.showOtherSummaries){
+                return(
+                    <tr className={i === 0 ? 'tr back-gray' : 'tr'} key={i}>
+                      <td className='td' >
+                        <img src={player.get("avatar")} className="avatar" />
+                      </td>
+                      <td className='td'><h2>{contribution}</h2></td>
+                      {punishment && 
+                      <>
+                        <td className='td'>
+                          <ListView
+                            punishments={punished}
+                            game={game}
+                            me={me}
+                          />
+                        </td>
+                        <td className='td' >
+                          <ListView
+                            punishments={punishedBy}
+                            game={game}
+                            me={me}
+                          />
+                        </td>
+                      </>
+                      } 
+                      <td className='td'><font color={roundPayoff > 0 ? "green":"red"}><h2>{roundPayoff}</h2></font></td>  
+                    </tr>
+                )
+                  }
+            })
+            }
           </tbody>
         </table>
       </div>
