2322ffaa7d7e8cf7ded7d63c93c8bc8cbd6d1cab
Mohammed Alsobay <mosobay@mit.edu>
2022-11-06 23:06:38 -0500

    update help modal with general game info

--- client/game/Header.jsx
+++ client/game/Header.jsx
@@ -18,6 +18,8 @@ export class HeaderWithTimer extends React.Component {
       roundNum += ` of ${game.treatment.numRounds}`;
     }
 
+    // roundNum += ` (${stage.displayName})`
+
     return (
       <div className="h-full w-full">
         <Header
@@ -50,7 +52,20 @@ function HelpModal({ done }) {
         >
           &times;
         </button>
-        <p>Some text in the Modal..</p>
+        <h1>Stages of the game</h1>
+        Each round of the game has three stages:
+        <ul>
+          <li><strong>Contribution:</strong> Each player decides how much of their grant money (20 coins each round) to contribute to the public fund. The public fund is then multiplied and divided equally among players.</li>
+          <li><strong>Outcome:</strong> The contributions and payoffs of each player are revealed.</li>
+          <li><strong>Summary:</strong> A summary of the round is presented, where you can mouse over an avatar to see details of that player's outcome.</li>
+        </ul>
+        <h1>Calculating payoffs from contributions:</h1>
+        <p>The amount each player receives from the public fund contributions is equal to (Total coins contributed) * (Multiplier) / (Number of players)</p>
+        <h1>Deducting from other players:</h1>
+        <p>During the outcome stage, you will have the option to deduct coins from other players. To do so, you must pay N coins per deduction, and each deduction will take away N coins from the targeted player.</p>
+        <h1>Calculating your total payoff for a single round:</h1>
+        <p>The total coins you gain for the round are calculated as (Number of coins remaining from the grant for the round) + (Your share from the public fund) - (Cost of any deductions you placed on others) - (Any deductions other players placed on you)</p>
+        <p>Your payoff for a single round is then added to the total coins in your piggy bank.</p>
       </div>
     </div>,
     document.querySelector("#modal-root")
