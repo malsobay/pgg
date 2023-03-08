58a24f9686a22a6e8495222fbb7a2a0efa9e7787
Nicolas Paton <nicolas.paton@gmail.com>
2023-03-08 07:10:50 +0000

    feat: reverse the order of the coins and the bowl in stage 1

--- client/components/AddCoins.jsx
+++ client/components/AddCoins.jsx
@@ -12,6 +12,9 @@ export function AddCoins({
   contributed,
   multiplier,
   onClick,
+  goDown = false,
+  addLabels = true,
+  useArrows = false,
   remainderMode = true,
   remainderDisplay10 = true,
   allOrNothing = false,
@@ -19,78 +22,131 @@ export function AddCoins({
   onSubmit = () => console.log("I'm done"),
   submitted = false,
 }) {
+  const headerComp = !submitted && (
+    <div className="pt-8">
+      <Label color="orange" size="md" snug>
+        {header}
+      </Label>
+    </div>
+  );
+
+  const purseComp = !submitted && (
+    <AddCoinsPurse
+      amount={purse - contributed}
+      footer={addLabels ? "Your pocket" : ""}
+    />
+  );
+
+  const arrows =
+    !submitted &&
+    (allOrNothing ? (
+      <AddCoinsAllOrNothingArrows
+        amount={allOrNothingAmount}
+        canAdd={purse - contributed > 0}
+        canRemove={contributed > 0}
+        onClick={onClick}
+        goDown={goDown}
+        useArrows={useArrows}
+      />
+    ) : remainderMode ? (
+      <AddCoinsArrows
+        goDown={goDown}
+        useArrows={useArrows}
+        canAdd1={purse - contributed > 0}
+        canAddMultiple={purse - contributed > 0}
+        addMultipleAmount={purse - contributed < 10 ? purse - contributed : 10}
+        displayAddMultipleAmount={
+          purse - contributed < 10 && !remainderDisplay10
+            ? purse - contributed
+            : 10
+        }
+        canRemove1={contributed > 0}
+        canRemoveMultiple={contributed > 0}
+        removeMultipleAmount={contributed >= 10 ? 10 : contributed}
+        displayRemoveMultipleAmount={
+          purse - contributed < 10 && !remainderDisplay10
+            ? purse - contributed
+            : 10
+        }
+        onClick={onClick}
+      />
+    ) : (
+      <AddCoinsArrows
+        goDown={goDown}
+        useArrows={useArrows}
+        canAdd1={purse - contributed > 0}
+        canAddMultiple={purse - contributed > 9}
+        canRemove1={contributed > 0}
+        canRemoveMultiple={contributed > 9}
+        onClick={onClick}
+      />
+    ));
+
+  const bowl = (
+    <div className="xl:w-44 2xl:w-52 pt-8">
+      <Bowl
+        money={contributed}
+        multiplier={multiplier}
+        footer={addLabels ? "Pot" : ""}
+      />
+    </div>
+  );
+
+  const button = !submitted && (
+    <div className="mt-8 w-full">
+      <Button onClick={onSubmit}>I'm done</Button>
+    </div>
+  );
+
+  const submittedComp = submitted && (
+    <div className="pt-8">
+      <Label color="gray" size="md" snug>
+        {submittedText}
+      </Label>
+    </div>
+  );
+
+  const footerComp = !submitted && (
+    <div className="pt-8">
+      <Label color="orange" size="md" snug>
+        {footer}
+      </Label>
+    </div>
+  );
+
+  let chilren = [];
+  if (goDown) {
+    chilren = [
+      headerComp,
+      purseComp,
+      arrows,
+      bowl,
+      button,
+      submittedComp,
+      footerComp,
+    ];
+  } else {
+    chilren = [
+      headerComp,
+      footerComp,
+      bowl,
+      arrows,
+      purseComp,
+      button,
+      submittedComp,
+    ];
+  }
+
   return (
     <div className="flex flex-col items-center text-center max-w-sm">
-      {!submitted && (
-        <Label color="orange" size="md" snug>
-          {header}
-        </Label>
-      )}
-      {!submitted && <AddCoinsPurse amount={purse - contributed} />}
-      {!submitted &&
-        (allOrNothing ? (
-          <AddCoinsAllOrNothingArrows
-            amount={allOrNothingAmount}
-            canAdd={purse - contributed > 0}
-            canRemove={contributed > 0}
-            onClick={onClick}
-          />
-        ) : remainderMode ? (
-          <AddCoinsArrows
-            canAdd1={purse - contributed > 0}
-            canAddMultiple={purse - contributed > 0}
-            addMultipleAmount={
-              purse - contributed < 10 ? purse - contributed : 10
-            }
-            displayAddMultipleAmount={
-              purse - contributed < 10 && !remainderDisplay10
-                ? purse - contributed
-                : 10
-            }
-            canRemove1={contributed > 0}
-            canRemoveMultiple={contributed > 0}
-            removeMultipleAmount={contributed >= 10 ? 10 : contributed}
-            displayRemoveMultipleAmount={
-              purse - contributed < 10 && !remainderDisplay10
-                ? purse - contributed
-                : 10
-            }
-            onClick={onClick}
-          />
-        ) : (
-          <AddCoinsArrows
-            canAdd1={purse - contributed > 0}
-            canAddMultiple={purse - contributed > 9}
-            canRemove1={contributed > 0}
-            canRemoveMultiple={contributed > 9}
-            onClick={onClick}
-          />
-        ))}
-      <div className="xl:w-44 2xl:w-52 pt-8">
-        <Bowl money={contributed} multiplier={multiplier} />
-      </div>
-      {!submitted && <Button onClick={onSubmit}>I'm done</Button>}
-      {submitted && (
-        <div className="pt-8">
-          <Label color="gray" size="md" snug>
-            {submittedText}
-          </Label>
-        </div>
-      )}
-      {!submitted && (
-        <div className="pt-8">
-          <Label color="orange" size="md" snug>
-            {footer}
-          </Label>
-        </div>
-      )}
+      {chilren}
     </div>
   );
 }
 
-export function AddCoinsPurse({ amount }) {
-  return (
-    <div className="flex h-24 overflow-hidden items-center">
+export function AddCoinsPurse({ amount, footer = "" }) {
+  const content = (
+    <div className="flex h-16 items-center">
       <div className="w-24 relative top-5">
         <Coin />
       </div>
@@ -99,9 +155,22 @@ export function AddCoinsPurse({ amount }) {
       </Label>
     </div>
   );
+
+  return footer ? (
+    <div className="flex h-24 flex-col overflow-hidden items-center">
+      {content}
+      <Label color="orange" size="md">
+        {footer}
+      </Label>
+    </div>
+  ) : (
+    content
+  );
 }
 
 export function AddCoinsArrows({
+  goDown = false,
+  useArrows = false,
   canAdd1 = false,
   canAddMultiple = false,
   addMultipleAmount = 10,
@@ -119,14 +188,22 @@ export function AddCoinsArrows({
         disabled={!canRemoveMultiple}
         onClick={() => onClick(-removeMultipleAmount)}
         dark
+        down={!goDown}
+        useArrows={useArrows}
       />
       <AddButton
         amount={1}
         disabled={!canRemove1}
         onClick={() => onClick(-1)}
+        down={!goDown}
+        useArrows={useArrows}
       />
       <div className="w-4 relative flex justify-center pointer-events-none">
-        <div className="absolute -top-16 2xl:-top-24 w-32 2xl:w-44 shrink-0 rotate-180">
+        <div
+          className={`absolute -top-16 2xl:-top-24 w-32 2xl:w-44 shrink-0 ${
+            goDown ? "rotate-180" : ""
+          }`}
+        >
           <Arrow></Arrow>
         </div>
       </div>
@@ -134,37 +211,46 @@ export function AddCoinsArrows({
         amount={1}
         disabled={!canAdd1}
         onClick={() => onClick(1)}
-        down
+        down={goDown}
+        useArrows={useArrows}
       />
       <AddButton
         amount={displayAddMultipleAmount}
         disabled={!canAddMultiple}
         onClick={() => onClick(addMultipleAmount)}
-        down
+        down={goDown}
         dark
+        useArrows={useArrows}
       />
     </div>
   );
 }
 
 export function AddCoinsAllOrNothingArrows({
+  goDown = false,
+  useArrows = false,
   amount,
   canAdd,
   canRemove,
   onClick,
 }) {
-  console.log(amount);
   return (
     <div className="h-24 2xl:h-48 flex items-center space-x-4">
       <AddButton
         amount={amount}
         disabled={!canRemove}
         onClick={() => onClick(-amount)}
+        down={!goDown}
         dark
         freeWidth
+        useArrows={useArrows}
       />
       <div className="w-4 relative flex justify-center pointer-events-none">
-        <div className="absolute -top-16 2xl:-top-20 w-32 2xl:w-44 shrink-0 rotate-180">
+        <div
+          className={`absolute -top-16 2xl:-top-20 w-32 2xl:w-44 shrink-0 ${
+            goDown ? "rotate-180" : ""
+          }`}
+        >
           <Arrow></Arrow>
         </div>
       </div>
@@ -172,9 +258,10 @@ export function AddCoinsAllOrNothingArrows({
         amount={amount}
         disabled={!canAdd}
         onClick={() => onClick(amount)}
-        down
+        down={goDown}
         dark
         freeWidth
+        useArrows={useArrows}
       />
     </div>
   );
@@ -185,6 +272,7 @@ function AddButton({
   down = false,
   dark = false,
   freeWidth = false,
+  useArrows = false,
   disabled,
   onClick,
 }) {
@@ -192,7 +280,7 @@ function AddButton({
     "h-14 relative flex items-center justify-center rounded-md outline outline-4 shadow-none";
 
   if (!freeWidth) {
-    className += " w-14";
+    className += " w-16";
   } else {
     className += " px-4 space-x-2";
   }
@@ -226,14 +314,29 @@ function AddButton({
     arrowClass += " -ml-4";
   }
 
+  let amountFinal = amount;
+  if (!useArrows) {
+    if (down) {
+      amountFinal = "-" + amount;
+    } else {
+      amountFinal = "+" + amount;
+    }
+  }
+
+  console.log(amountFinal);
+
   return (
     <button className={className} onClick={onClick} disabled={disabled}>
-      <div className={arrowClass}>
-        <Arrow color="white" full />
-      </div>
-      <div className="w-8 flex justify-center shrink-0 -ml-2">
+      {useArrows ? (
+        <div className={arrowClass}>
+          <Arrow color="white" full />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="flex justify-center shrink-0 whitespace-nowrap">
         <Label color="white" size="lg" tight>
-          {amount}
+          {amountFinal}
         </Label>
       </div>
     </button>
