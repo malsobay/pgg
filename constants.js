760378a1c0202f2762be973ac6a9f11d9123e484
Mohammed Alsobay <mosobay@mit.edu>
2023-04-18 18:00:36 -0400

    update constants with timeout params for lobby idle

--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
 import { dev } from "./dev.js";
 export const warningTime = dev ? 50000 : 15;
 export const idleTimeDifferentTab = dev ? 50000 : 25;
+export const lobbyWarningTime = dev ? 50000 : 30;
+export const lobbyIdleTimeDifferentTab = dev ? 50000 : 5;
 export const numStagesRemoveOffline = 3; 
 export const experimentDate = 1681539730000;
